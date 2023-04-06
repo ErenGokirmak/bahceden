f7d0ec880f07c4bedfce63d5d77680cf5a0a8213
Eren <sukruerengokirmak@gmail.com>
2023-04-07 02:35:47 +0300

    v0.0.2: Fixed package name, added color palette * Changed package name from com.example.bahceden to com.swifties.bahceden * Added colors to values/colors.xml

--- app/src/androidTest/java/com/swifties/bahceden/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/swifties/bahceden/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.swifties.bahceden;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+        assertEquals("com.swifties.bahceden", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/swifties/bahceden/ExampleUnitTest.java
+++ app/src/test/java/com/swifties/bahceden/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.myapplication;
+package com.swifties.bahceden;
 
 import org.junit.Test;
 
