0a937f3ecc955da5f19b7e108b8a9e4e3ae0bf5d
afikbae <akfshn.2004@gmail.com>
2023-04-15 16:29:40 +0300

    added temporary functionality to recycler views in homepage

--- app/src/main/java/com/swifties/bahceden/HomeActivity.java
+++ app/src/main/java/com/swifties/bahceden/HomeActivity.java
@@ -1,14 +1,54 @@
 package com.swifties.bahceden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private RecyclerView categoriesRV;
+    private RecyclerView hotSalesRV;
+    private RecyclerView newArrivalsRV;
+    private RecyclerView.Adapter categoriesAdapter;
+    private RecyclerView.Adapter arrivalsAdapter;
+    private RecyclerView.Adapter hotSalesAdapter;
+
+    RecyclerView.LayoutManager cLayoutManager;
+    RecyclerView.LayoutManager aLayoutManager;
+    RecyclerView.LayoutManager hLayoutManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        categoriesRV = (RecyclerView) findViewById(R.id.categoriesRV);
+        categoriesRV.setHasFixedSize(true);
+        cLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        categoriesRV.setLayoutManager(cLayoutManager);
+        categoriesAdapter = new CategoriesAdapter();
+        categoriesRV.setAdapter(categoriesAdapter);
+
+        hotSalesRV = (RecyclerView) findViewById(R.id.hotSalesRV);
+        hotSalesRV.setHasFixedSize(true);
+        hLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        hotSalesRV.setLayoutManager(hLayoutManager);
+        hotSalesAdapter = new CategoriesAdapter();
+        hotSalesRV.setAdapter(hotSalesAdapter);
+
+        newArrivalsRV = (RecyclerView) findViewById(R.id.newArrivalsRV);
+        newArrivalsRV.setHasFixedSize(true);
+        aLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        newArrivalsRV.setLayoutManager(aLayoutManager);
+        arrivalsAdapter = new CategoriesAdapter();
+        newArrivalsRV.setAdapter(arrivalsAdapter);
+
+
+
     }
 }
