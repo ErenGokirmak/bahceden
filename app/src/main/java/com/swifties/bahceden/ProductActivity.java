361726b6e3ffddd97ff52ddb93df65c43e1b6f73
afikbae <akfshn.2004@gmail.com>
2023-04-20 01:43:15 +0300

    fixed typo

--- app/src/main/java/com/swifties/bahceden/ProductActivity.java
+++ app/src/main/java/com/swifties/bahceden/ProductActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class productActivity extends AppCompatActivity {
+public class ProductActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
