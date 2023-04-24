a28079925e5e94e49ceca78a460829944164daa2
Erfan-FK <erfanfarhangkia@gmail.com>
2023-04-24 19:32:46 +0300

    New implementation.

--- app/src/main/java/com/swifties/bahceden/fragments/EnterMailFragment.java
+++ app/src/main/java/com/swifties/bahceden/fragments/EnterMailFragment.java
@@ -1,4 +1,4 @@
-package com.swifties.bahceden;
+package com.swifties.bahceden.fragments;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ChooseActionFragment extends Fragment {
+import com.swifties.bahceden.R;
+
+public class EnterMailFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_choose_action, container, false);
+        return inflater.inflate(R.layout.fragment_enter_mail, container, false);
     }
 }
