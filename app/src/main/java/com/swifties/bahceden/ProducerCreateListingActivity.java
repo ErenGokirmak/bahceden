96e63b696aaeaa6b603d2c7b32684291f0b73b3f
Eren <sukruerengokirmak@gmail.com>
2023-04-24 21:37:16 +0300

    Redesigned Producer Add Listing

--- app/src/main/java/com/swifties/bahceden/ProducerCreateListingActivity.java
+++ app/src/main/java/com/swifties/bahceden/ProducerCreateListingActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProducerAddListingActivity extends AppCompatActivity {
+public class ProducerCreateListingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_producer_add_listing);
+        setContentView(R.layout.activity_producer_create_listing);
     }
 }
