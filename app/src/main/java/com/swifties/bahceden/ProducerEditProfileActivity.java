b19c683317f7bcc584c31cb07096c7c6747823a2
afikbae <akfshn.2004@gmail.com>
2023-04-26 03:37:31 +0300

    added producer edit profile

---
+++ app/src/main/java/com/swifties/bahceden/ProducerEditProfileActivity.java
@@ -0,0 +1,14 @@
+package com.swifties.bahceden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProducerEditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_producer_edit_profile);
+    }
+}
