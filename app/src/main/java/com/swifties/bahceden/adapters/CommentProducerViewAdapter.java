b35692dc5af9091e645333a1875a7c54a6048c07
afikbae <akfshn.2004@gmail.com>
2023-04-29 22:13:53 +0300

    same as before

--- app/src/main/java/com/swifties/bahceden/adapters/CommentProducerViewAdapter.java
+++ app/src/main/java/com/swifties/bahceden/adapters/CommentProducerViewAdapter.java
@@ -1,8 +1,14 @@
 package com.swifties.bahceden.adapters;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -10,7 +16,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.swifties.bahceden.R;
 
-public class NewReviewsAdapter extends RecyclerView.Adapter<NewReviewsAdapter.ViewHolder> {
+public class CommentProducerViewAdapter extends RecyclerView.Adapter<CommentProducerViewAdapter.ViewHolder> {
 
     @NonNull
     @Override
@@ -22,7 +28,28 @@ public class NewReviewsAdapter extends RecyclerView.Adapter<NewReviewsAdapter.Vi
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.replyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.replyEdit.setVisibility(View.VISIBLE);
+                holder.replyButton.setVisibility(View.GONE);
+                Animation animation = AnimationUtils.loadAnimation(v.getContext(), R.anim.pop);
+                holder.replyEdit.startAnimation(animation);
+                holder.likeCount.setVisibility(View.GONE);
+                holder.commentDate.setVisibility(View.GONE);
+            }
+        });
 
+        holder.replyEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                holder.replyEdit.setVisibility(View.GONE);
+                holder.replyButton.setVisibility(View.VISIBLE);
+                holder.likeCount.setVisibility(View.VISIBLE);
+                holder.commentDate.setVisibility(View.VISIBLE);
+                return true;
+            }
+        });
     }
 
     @Override
@@ -32,8 +59,19 @@ public class NewReviewsAdapter extends RecyclerView.Adapter<NewReviewsAdapter.Vi
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        EditText replyEdit;
+        ImageView replyButton;
+
+        TextView likeCount;
+
+        TextView commentDate;
         public ViewHolder(View view) {
             super(view);
+
+            replyEdit = view.findViewById(R.id.commentReplyEditText);
+            replyButton = view.findViewById(R.id.commentReplyButton);
+            likeCount = view.findViewById(R.id.likeCount);
+            commentDate = view.findViewById(R.id.commentDate);
 
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
