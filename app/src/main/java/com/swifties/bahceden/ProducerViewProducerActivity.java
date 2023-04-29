db1d7b88eda6d1ee706ca851a97f46f90e55a1be
afikbae <akfshn.2004@gmail.com>
2023-04-29 20:49:51 +0300

    customerviewproducer and producerviewproducer

--- app/src/main/java/com/swifties/bahceden/ProducerViewProducerActivity.java
+++ app/src/main/java/com/swifties/bahceden/ProducerViewProducerActivity.java
@@ -1,14 +1,39 @@
 package com.swifties.bahceden;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.swifties.bahceden.adapters.ProducerAdapter;
+import com.swifties.bahceden.classes.GridSpacingItemDecoration;
 
 public class ProducerViewProducerActivity extends AppCompatActivity {
+    private RecyclerView producerRV;
+    private RecyclerView.Adapter ProducerAdapter;
+    RecyclerView.LayoutManager producerLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producer_view_producer);
+
+        producerRV = (RecyclerView) findViewById(R.id.producerViewProducerProductsRV);
+
+        int spanCount = 2;
+        int dp_spacing = 30;
+        int spacing = Math.round(dp_spacing * getResources().getDisplayMetrics().density);
+        boolean includeEdge = false;
+        producerRV.addItemDecoration(new GridSpacingItemDecoration(spanCount, spacing, includeEdge));
+
+        producerRV.setHasFixedSize(true);
+        producerLayoutManager = new GridLayoutManager(this, 2);
+
+        producerRV.setLayoutManager(producerLayoutManager);
+        ProducerAdapter = new ProducerAdapter();
+        producerRV.setAdapter(ProducerAdapter);
     }
 }
