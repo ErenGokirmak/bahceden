31c0053545d0aeaa291bbbecb32a23757e0bd92a
Eren <sukruerengokirmak@gmail.com>
2023-05-02 23:32:02 +0300

    implemented profile buttons (EditProfileActivity doesn't work somehow)
    if anyone can find out why, please be sure to fix it

--- app/src/main/java/com/swifties/bahceden/SplashActivity.java
+++ app/src/main/java/com/swifties/bahceden/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, CustomerMainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, ProducerMainActivity.class);
                 startActivity(intent);
                 finish();
             }
--- app/src/main/java/com/swifties/bahceden/fragments/ProducerProfileFragment.java
+++ app/src/main/java/com/swifties/bahceden/fragments/ProducerProfileFragment.java
@@ -1,5 +1,6 @@
 package com.swifties.bahceden.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,57 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
+import com.swifties.bahceden.CustomerEditProfileActivity;
+import com.swifties.bahceden.ProducerAnalyticsActivity;
+import com.swifties.bahceden.ProducerEditProfileActivity;
 import com.swifties.bahceden.R;
+import com.swifties.bahceden.SecurityProfileActivity;
+
+import java.security.Security;
 
 public class ProducerProfileFragment extends Fragment {
 
+    LinearLayout editProfileButton;
+    LinearLayout analyticsButton;
+    LinearLayout securityButton;
+    LinearLayout logOutButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_producer_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_producer_profile, container, false);
+
+        editProfileButton = view.findViewById(R.id.producerProfileEditProfileButton);
+        analyticsButton = view.findViewById(R.id.producerProfileAnalyticsButton);
+        securityButton = view.findViewById(R.id.producerProfileSecurityButton);
+        logOutButton = view.findViewById(R.id.producerProfileLogOutButton);
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProducerEditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        analyticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ProducerAnalyticsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        securityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), SecurityProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // TODO: Log Out Button isn't implemented as we're not logged in at the moment.
+
+        return view;
     }
 }
