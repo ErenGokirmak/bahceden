4d5dd389fc2011b8d777b4b552471f32ec395284
Eren <sukruerengokirmak@gmail.com>
2023-05-24 16:18:02 +0300

    idea for search activity

--- app/src/main/java/com/swifties/bahceden/fragments/CustomerSearchFragment.java
+++ app/src/main/java/com/swifties/bahceden/fragments/CustomerSearchFragment.java
@@ -3,22 +3,23 @@ package com.swifties.bahceden.fragments;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.PopupWindow;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.PopupWindow;
-
 import com.swifties.bahceden.R;
+import com.swifties.bahceden.data.RetrofitService;
 
 import java.util.ArrayList;
 
@@ -27,6 +28,8 @@ public class CustomerSearchFragment extends Fragment {
     ArrayList<String> searchHistoryList = new ArrayList<>();
     PopupWindow searchHistoryPopup;
     RecyclerView productSearchRV;
+    Button filtersButton, sortByButton;
+    ImageView searchButton;
     EditText searchHistoryEditText;
 
     @Override
@@ -42,6 +45,28 @@ public class CustomerSearchFragment extends Fragment {
         // Create the RecyclerView and its adapter
         productSearchRV = view.findViewById(R.id.customerSearchResultsRV);
         productSearchRV.setHasFixedSize(false);
+
+        searchButton = view.findViewById(R.id.customerSearchImg);
+        filtersButton = view.findViewById(R.id.customerSearchFiltersButton);
+        sortByButton = view.findViewById(R.id.customerSearchSortByButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchKeyword = String.valueOf(searchHistoryEditText.getText());
+                // TODO: Make filters and sort by work, and add them to the request
+                //  (probably make filters button into a spinner)
+
+                // TODO:
+                //  if ([filter is "products"]
+                //      RetrofitService.getApi(ProductApi.class)...
+                //  else if ([filter is "producers"])
+                //      RetrofitService.getApi(ProducerApi.class)...
+                //  else if ([filter is "all"])
+                //      List<Producer> producers; (fill this using a request)
+                //      List<Product> products; (again, with ta request)
+                //      productSearchRV.setAdapter(
+            }
+        });
 
         // Fill the search history list
         fillSearchHistory();
