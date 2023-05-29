048e1f4f360851d59d784dea997acbb653855145
afikbae <akfshn.2004@gmail.com>
2023-05-30 00:40:38 +0300

    customer analytics, waiting for backend

--- app/src/main/java/com/swifties/bahceden/activities/CustomerAnalyticsActivity.java
+++ app/src/main/java/com/swifties/bahceden/activities/CustomerAnalyticsActivity.java
@@ -12,7 +12,10 @@ import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.swifties.bahceden.R;
+import com.swifties.bahceden.data.AuthUser;
+import com.swifties.bahceden.data.RetrofitService;
 import com.swifties.bahceden.databinding.ActivityCustomerAnalyticsBinding;
+import com.swifties.bahceden.models.Order;
 
 import java.util.ArrayList;
 
@@ -22,33 +25,44 @@ public class CustomerAnalyticsActivity extends AppCompatActivity {
     private ImageView backButton;
     private PieChart consumerChart;
     private ActivityCustomerAnalyticsBinding binding;
+    int marketTotal;
+    double bahcedenTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCustomerAnalyticsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         binding.customerAnalyticsBackButton.setOnClickListener(view -> CustomerAnalyticsActivity.super.onBackPressed());
-        Toast.makeText(this, "AFAIK this page won't exist", Toast.LENGTH_SHORT).show();
 
-        consumerChart = binding.customerAnalyticsConsumerChart;
+        // TODO get the market prices of the customer's orders and calculate the total.
+        // TODO set it to marketTotal.
 
+        if (AuthUser.getCustomer().getOrders().stream().anyMatch(o -> o.getStatus() != Order.OrderStatus.IN_CART))
+            bahcedenTotal = AuthUser.getCustomer().getOrders().stream().filter(o -> o.getStatus() != Order.OrderStatus.IN_CART).map(Order::getTotalPrice).reduce(Double::sum).get();
+
+        setViews(); // TODO call this after the calculations.
+    }
+
+    private void setViews ()
+    {
         ArrayList<PieEntry> dataEntries = new ArrayList<>();
-        dataEntries.add(new PieEntry(10, "Benefits"));
-        dataEntries.add(new PieEntry(200, "Costs"));
+        dataEntries.add(new PieEntry((int) (marketTotal - bahcedenTotal), "Benefits"));
+        dataEntries.add(new PieEntry((int) bahcedenTotal, "Costs"));
 
         PieDataSet pieDataSet = new PieDataSet(dataEntries, "Monthly Data");
         pieDataSet.setColors(getResources().getColor(R.color.plus_green, null), getResources().getColor(R.color.minus_red, null));
         pieDataSet.setValueTextColor(Color.WHITE);
         pieDataSet.setValueTextSize(16);
-
         PieData pieData = new PieData(pieDataSet);
 
-        consumerChart.setData(pieData);
-        consumerChart.setCenterText("Monthly Data");
-        consumerChart.getDescription().setEnabled(false);
-        consumerChart.animate();
-    }
+        binding.customerAnalyticsConsumerChart.setData(pieData);
+        binding.customerAnalyticsConsumerChart.setCenterText("Monthly Data");
+        binding.customerAnalyticsConsumerChart.getDescription().setEnabled(false);
+        binding.customerAnalyticsConsumerChart.animate();
 
+        binding.customerAnalyticsBenefitValue.setText(String.valueOf(marketTotal - bahcedenTotal));
+        binding.customerAnalyticsCostsValue.setText(String.valueOf(bahcedenTotal));
+        binding.totalCostText.setText(String.format(getString(R.string.analytics_text), marketTotal));
+    }
 }
